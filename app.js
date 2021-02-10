bcf1220372ef264330a5f0d5f8c70fbafcf778a8
Krystian Otto <otto.krystiann@gmail.com>
2021-02-10 14:05:26 +0100

    basics of vue

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const app = Vue.createApp({
   // template: '<h2>Hey</h2>'
   data() {
     return {
+      showTitle: true,
       title: 'The dynamic title',
       author: 'an author name',
+      age: 20,
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.title = "bla blik bak";
+    },
+    toggleTitleVisibility() {
+      this.showTitle = !this.showTitle;
     }
   }
 });
